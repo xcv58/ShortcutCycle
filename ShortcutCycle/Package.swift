81cc06803877e68293efdf28d1fee64b0320e32a
xcv58 <i@xcv58.com>
2026-01-26 20:29:58 -0500

    Fix: Update KeyboardShortcuts dependency URL to include .git suffix for Xcode Cloud

--- ShortcutCycle/Package.swift
+++ ShortcutCycle/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .executable(name: "ShortcutCycle", targets: ["ShortcutCycle"])
     ],
     dependencies: [
-        .package(url: "https://github.com/sindresorhus/KeyboardShortcuts", from: "2.0.0")
+        .package(url: "https://github.com/sindresorhus/KeyboardShortcuts.git", from: "2.0.0")
     ],
     targets: [
         .executableTarget(
